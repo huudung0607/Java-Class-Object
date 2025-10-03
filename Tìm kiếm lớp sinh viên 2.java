169d8f2fe97b9a62d05858c9408a0964e813e75b
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-03 09:20:02 +0700

    Create Tìm kiếm lớp sinh viên 2.java

---
+++ Tìm kiếm lớp sinh viên 2.java
@@ -0,0 +1,67 @@
+import com.sun.source.tree.Tree;
+import java.lang.reflect.Array;
+import java.net.Inet4Address;
+import java.util.*;
+import java.lang.*;
+import java.util.stream.Collectors;
+class Student{
+    private String msv, hoTen,ngaySinh,lop;
+    private double gpa;
+    Student(){}
+    Student(String msv, String hoTen, String ngaySinh, String lop, double gpa){
+        this.msv = msv;
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.lop = lop;
+        this.gpa = gpa;
+    }
+    public String getLop(){
+        return lop;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+    public void chuanHoaTen(){
+        String []arr = hoTen.trim().toLowerCase().split("\\s+");
+        String res = "";
+        for(var x : arr){
+            res += Character.toUpperCase(x.charAt(0));
+            for(int j = 1;j < x.length();j++){
+                res += Character.toLowerCase(x.charAt(j));
+            }
+            res += " ";
+        }
+        hoTen = res.trim();
+    }
+    public void chuanHoaNS(){
+        if (ngaySinh.length() >= 2 && ngaySinh.charAt(1) == '/') {
+            ngaySinh = "0" + ngaySinh;
+        }
+        if (ngaySinh.length() >= 5 && ngaySinh.charAt(4) == '/') {
+            ngaySinh = ngaySinh.substring(0, 3) + "0" + ngaySinh.substring(3);
+        }
+    }
+    public void in(){
+        System.out.printf("%s %s %s %s %.2f", msv,hoTen,ngaySinh,lop,gpa);
+    }
+}
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = Integer.parseInt(sc.nextLine());
+        List<Student> list = new ArrayList<>();
+        for(int i = 0;i < n;i++){
+            Student s = new Student(sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextDouble());
+            sc.nextLine();
+            s.chuanHoaNS(); s.chuanHoaTen();
+            list.add(s);
+        }
+        for(var i : list){
+            if(i.getGpa() >= 2.5){
+                i.in();
+                System.out.println();
+            }
+        }
+    }
+}
